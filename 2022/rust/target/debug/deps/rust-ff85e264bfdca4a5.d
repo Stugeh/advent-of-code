f4eb739201506e98fda8beeb5e6b324377f983b4
tuukka veteli <tuukka.veteli@solita.fi>
2023-07-02 15:39:24 +0300

    removed files to ignore

--- 2022/rust/target/debug/deps/rust-ff85e264bfdca4a5.d
+++
@@ -1,7 +0,0 @@
-/home/tuukka/advent-of-code/2022/rust/target/debug/deps/rust-ff85e264bfdca4a5: src/main.rs src/day01.rs src/day02.rs
-
-/home/tuukka/advent-of-code/2022/rust/target/debug/deps/rust-ff85e264bfdca4a5.d: src/main.rs src/day01.rs src/day02.rs
-
-src/main.rs:
-src/day01.rs:
-src/day02.rs:
--- 2022/rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-/home/tuukka/advent-of-code/2022/rust/target/debug/rust: /home/tuukka/advent-of-code/2022/rust/src/day01.rs /home/tuukka/advent-of-code/2022/rust/src/day02.rs /home/tuukka/advent-of-code/2022/rust/src/main.rs
