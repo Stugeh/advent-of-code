b4e3c30f3eb48ab6e6ab78cf96c01cac68544025
tuukka veteli <tuukkaveteli@gmail.com>
2023-12-09 21:34:51 +0200

    finished day 1 part 1

--- 2023/rust/src/day_01.rs
+++ 2023/rust/src/day_01.rs
@@ -12,7 +12,49 @@
 // total = 142
 //
 // Get total.
+//
+//--- Part Two ---
+// It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight,
+// and nine also count as valid "digits".
 
 pub fn solution(input: Vec<String>) {
-    input.iter().for_each(|input_line| println!("{input_line}"));
+    let mut total: i32 = 0;
+
+    for input_line in &input {
+        let line_bytes = input_line.as_bytes();
+        let last_idx = input_line.len() - 1;
+        let mut idx_offset = 0;
+        let mut last_num = '_';
+        let mut first_num = '_';
+
+        while last_num == '_' || first_num == '_' {
+            if last_num == '_' {
+                let ch = line_bytes[last_idx - idx_offset] as char;
+                if ch.is_numeric() {
+                    last_num = ch;
+                }
+            }
+            if first_num == '_' {
+                let ch = line_bytes[idx_offset] as char;
+                if ch.is_numeric() {
+                    first_num = ch;
+                }
+            }
+
+            idx_offset += 1;
+        }
+
+        if last_num == '_' {
+            last_num = first_num;
+        } else if first_num == '_' {
+            first_num = last_num;
+        }
+
+        let result = format!("{}{}", first_num, last_num);
+        println!("{}", result);
+
+        total += result.parse::<i32>().unwrap_or(0);
+    }
+
+    println!("{}", total);
 }
