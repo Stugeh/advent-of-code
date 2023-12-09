8dbe8737ad124fb241ec45b12eb18fb4362cefbb
tuukka veteli <tuukkaveteli@gmail.com>
2023-12-09 20:10:38 +0200

    initialized day 1 exercise

---
+++ 2023/rust/src/day_01.rs
@@ -0,0 +1,18 @@
+// --- Day 1: Trebuchet?! ---
+// Calibration document is scrambled.
+//
+// Each line should contain a specific calibration value that needs to be recovered.
+// Calibration value on each line can be found by combining the first digit and the last digit to form a single two-digit number.
+//
+// For example:
+// 1abc2        12
+// pqr3stu8vwx  38
+// a1b2c3d4e5f  15
+// treb7uchet   77
+// total = 142
+//
+// Get total.
+
+pub fn solution(input: Vec<String>) {
+    input.iter().for_each(|input_line| println!("{input_line}"));
+}
--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -1,3 +1,32 @@
+use std::fs;
+
+pub mod day_01;
+// pub mod day_02;
+// pub mod day_03;
+// pub mod day_04;
+
+fn get_input_lines(day: &str) -> Vec<String> {
+    let input_lines = fs::read_to_string(format!("../inputs/day_{}", day));
+
+    if let Ok(input_lines) = input_lines {
+        input_lines.lines().map(String::from).collect()
+    } else {
+        Vec::new()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = get_input_lines("01");
+    // let args: Vec<String> = env::args().collect();
+    //
+    // let day = args[1].clone();
+    //
+    // match day.as_str() {
+    //     "01" => day_01::solution(input),
+    //     // "02" => day02::get_score(input),
+    //     // "03" => day03::organize_rucksacks(input),
+    //     // "04" => day04::get_consumed_ranges_count(input),
+    //     &_ => {}
+    // };
+    day_01::solution(input);
 }
