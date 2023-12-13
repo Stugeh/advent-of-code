ad3236fb2ec75f7e441a21fe32d14ca542a22ff5
tuukka veteli <tuukkaveteli@gmail.com>
2023-12-13 22:19:55 +0200

    init day 2

---
+++ 2023/rust/src/day_02.rs
@@ -0,0 +1,20 @@
+// --- Day 2: Cube Conundrum ---
+// Cubes are either red, green or blue.
+// In a bag there are a random number of cubes of each color.
+//
+// Elf will reach into the bag, grab a handful of random cubes, show them to you, and then put them back in the bag.
+// He'll do this a few times per game.
+//
+// Each game is listed with its ID number (like the 11 in Game 11: ...) followed by a semicolon-separated list of subsets of cubes that were revealed from the bag (like 3 red, 5 green, 4 blue).
+//
+// For example, the record of a few games might look like this:
+//
+// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+// Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+// Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+// Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+// Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+//
+// What is the sum of the IDs of games possible with 12 red cubes, 13 green cubes, and 14 blue cubes?
+
+pub fn solution(mut input: Vec<String>) {}
--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 pub mod day_01;
-// pub mod day_02;
+pub mod day_02;
 // pub mod day_03;
 // pub mod day_04;
 
@@ -16,17 +16,7 @@ fn get_input_lines(day: &str) -> Vec<String> {
 }
 
 fn main() {
-    let input = get_input_lines("01");
-    // let args: Vec<String> = env::args().collect();
-    //
-    // let day = args[1].clone();
-    //
-    // match day.as_str() {
-    //     "01" => day_01::solution(input),
-    //     // "02" => day02::get_score(input),
-    //     // "03" => day03::organize_rucksacks(input),
-    //     // "04" => day04::get_consumed_ranges_count(input),
-    //     &_ => {}
-    // };
-    day_01::solution(input);
+    let input = get_input_lines("02");
+    // day_01::solution(input);
+    day_02::solution(input)
 }
